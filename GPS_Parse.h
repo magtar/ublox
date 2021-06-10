98e6e597449e2dd886643eaeb8efb059af375ff8
magtar <mechatron2000@gmail.com>
2021-06-10 14:30:11 -0600

    fixed parser

--- GPS_Parse.h
+++ GPS_Parse.h
@@ -1,6 +1,7 @@
 #pragma once
 #include "GPS_MSG.h"
 #include "Arduino.h"
+#include "mbed.h"
 
 class GPS_Parse
 {
@@ -8,13 +9,13 @@ class GPS_Parse
         GPS_Parse(char *rx_buffer);
         ~GPS_Parse();
         void getc();
-        bool checksum();
+        void checksum();
         bool header_found;
         short int buff_count;
         char gps_buff[255];
         char *RX;
         uint16_t payloadLen;
-        uint8_t readnum;
+        uint16_t readnum;
         uint8_t CK_A;
         uint8_t CK_B;
 };
